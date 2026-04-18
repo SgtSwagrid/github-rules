69857dc6c0073617ec29ac072e39e99affef5a59
Alec Dorrington <alecdorrington@gmail.com>
2026-04-18 19:30:44 +0200

    Added debug statements.

--- scripts/import-settings.sh
+++ scripts/import-settings.sh
@@ -15,17 +15,20 @@ if [ ! -f "$SETTINGS_FILE" ]; then
   exit 0
 fi
 
-jq 'del(._comment) | del(.actions_enabled, .actions_allowed, .actions_default_workflow_permissions, .actions_can_approve_pull_request_reviews)' "$SETTINGS_FILE" \
-  | gh api "repos/$GITHUB_REPOSITORY" --method PATCH --input - > /dev/null
+REPO=$(jq 'del(._comment) | del(.actions_enabled, .actions_allowed, .actions_default_workflow_permissions, .actions_can_approve_pull_request_reviews) | with_entries(select(.value != null))' "$SETTINGS_FILE")
+echo "Importing repository settings..."
+echo "$REPO" | gh api "repos/$GITHUB_REPOSITORY" --method PATCH --input - > /dev/null
 
 ACTIONS_PERMISSIONS=$(jq '{enabled: .actions_enabled, allowed_actions: .actions_allowed} | with_entries(select(.value != null))' "$SETTINGS_FILE")
 if [ "$ACTIONS_PERMISSIONS" != "{}" ]; then
+  echo "Importing Actions permissions..."
   echo "$ACTIONS_PERMISSIONS" \
     | gh api "repos/$GITHUB_REPOSITORY/actions/permissions" --method PUT --input - > /dev/null
 fi
 
 ACTIONS_WORKFLOW=$(jq '{default_workflow_permissions: .actions_default_workflow_permissions, can_approve_pull_request_reviews: .actions_can_approve_pull_request_reviews} | with_entries(select(.value != null))' "$SETTINGS_FILE")
 if [ "$ACTIONS_WORKFLOW" != "{}" ]; then
+  echo "Importing Actions workflow permissions..."
   echo "$ACTIONS_WORKFLOW" \
     | gh api "repos/$GITHUB_REPOSITORY/actions/permissions/workflow" --method PUT --input - > /dev/null
 fi
